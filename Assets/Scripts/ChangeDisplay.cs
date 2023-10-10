9cbbd8f9e36b8511245b72ad70ab32c985ee7781
wenqian157 <44199787+wenqian157@users.noreply.github.com>
2023-10-10 08:23:21 +0200

    update

--- Assets/Scripts/ChangeDisplay.cs
+++ Assets/Scripts/ChangeDisplay.cs
@@ -8,11 +8,13 @@ using UnityEngine.XR.ARFoundation;
 
 public class ChangeDisplay : MonoBehaviour
 {
+    public TMP_Dropdown dropDown;
     ARTrackedImageManager m_TrackedImageManager;
 
     void Awake()
     {
         m_TrackedImageManager = GetComponent<ARTrackedImageManager>();
+        dropDown.onValueChanged.AddListener(delegate { ClickChangeDisplay(dropDown); });
     }
 
     void OnEnable()
@@ -34,7 +36,20 @@ public class ChangeDisplay : MonoBehaviour
             trackedImage.transform.localPosition = new Vector3(trackedImage.transform.localPosition.x, 0.005f, trackedImage.transform.localPosition.z);
         }
     }
-
+    public void ClickChangeDisplay(TMP_Dropdown myDropDown)
+    {
+        TextManager.Instance.debugText.text = "change mats";
+        string name = myDropDown.value.ToString();
+        TextManager.Instance.debugText.text = name;
+        Material mat = (Material)Resources.Load(name);
+        GameObject parent = GameObject.FindGameObjectWithTag("Model");
+        if (parent != null)
+        {
+            GameObject terrain = parent.transform.GetChild(0).gameObject;
+            terrain.GetComponent<MeshRenderer>().material = mat;
+        }
+        else TextManager.Instance.debugText.text = "didnt find material";
+    }
     public void ClickChangeMat(Material mat)
     {
         GameObject parent = GameObject.FindGameObjectWithTag("Model");
